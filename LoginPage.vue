da23c13da88023941cabf2635717d5960b1263ad
Imam Abdillah <92290516+imamabdillah@users.noreply.github.com>
2024-06-05 22:12:52 +0700

    m

---
+++ LoginPage.vue
@@ -0,0 +1,134 @@
+<template>
+  <q-layout>
+    <q-page-container>
+      <q-page class="flex bg-image flex-center">
+        <q-card v-bind:style="$q.screen.lt.sm ? { width: '80%' } : { width: '30%' }" class="custom-card">
+          <q-card-section>
+            <div class="text-center q-pt-lg">
+              <q-img src="/public/images/prisca logo.png" style="width: 103px; height: 103px; margin: 0 auto" />
+            </div>
+          </q-card-section>
+          <q-card-section>
+            <q-form class="q-gutter-md">
+              <q-input filled v-model="email" label="Email" lazy-rules />
+              <q-input filled v-model="password" :type="showPassword ? 'text' : 'password'" label="Password" lazy-rules>
+                <template v-slot:append>
+                  <q-icon :name="showPassword ? 'visibility_off' : 'visibility'" class="cursor-pointer"
+                    @click="togglePassword" />
+                </template>
+              </q-input>
+              <div class="text-red-9" v-if="errorMessage">
+                {{ errorMessage }}
+              </div>
+              <div class="text-center">
+                <q-btn label="Login" @click="login" type="button" color="primary" class="q-ma-xs q-ma-sm" size="lg"
+                  style="
+                    width: 100%;
+                    min-width: 200px;
+                    max-width: 550px;
+                    margin: auto;
+                  " />
+              </div>
+              <div class="text-center">
+                <p>
+                  Register as
+                  <span class="register-link" @click="goToRegisterBuyer">Vendor</span>
+                  /
+                  <span class="register-link" @click="goToRegisterBuyer">Buyer</span>
+                </p>
+              </div>
+            </q-form>
+          </q-card-section>
+        </q-card>
+      </q-page>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+import axios from "axios";
+export default {
+  name: "LoginPage",
+  setup() {
+    const router = useRouter();
+    const email = ref("");
+    const password = ref("");
+    const errorMessage = ref("");
+    const showPassword = ref(false);
+    console.log(process.env.VUE_APP_API_URL);
+    const apiUrl = process.env.VUE_APP_API_URL;
+    const login = async () => {
+      try {
+        const response = await axios.post(
+          "http://192.168.16.70:8000/api/login",
+          {
+            email: email.value,
+            password: password.value,
+          }
+        );
+
+        if (response.status === 200) {
+          const token = response.data.token;
+          const userLevel = response.data.user.role.name;
+          const userId = response.data.user.id;
+          localStorage.setItem("token", token);
+          localStorage.setItem("userLevel", userLevel);
+          localStorage.setItem("userId", userId);
+          if (userLevel === "Departemen") {
+            router.push("/dashboard-Admin");
+          } else if (userLevel === "user_approval") {
+            router.push("/purchase-request-approval");
+          } else if (userLevel === "company") {
+            router.push("/dashboard-Admin");
+          }
+        } else {
+          console.error("Error during login:", response.statusText);
+          errorMessage.value = "Invalid email or password";
+        }
+      } catch (error) {
+        console.error("Error during login:", error);
+        errorMessage.value = "Invalid email or password";
+      }
+    };
+
+    const togglePassword = () => {
+      showPassword.value = !showPassword.value;
+    };
+
+    const goToRegisterBuyer = () => {
+      router.push("/register-buyer");
+    };
+
+    return {
+      email,
+      password,
+      errorMessage,
+      showPassword,
+      login,
+      togglePassword,
+      goToRegisterBuyer,
+    };
+  },
+};
+</script>
+
+<style>
+.bg-image {
+  background-image: linear-gradient(135deg, #365486 0%, #365486 100%);
+}
+
+.text-red-9 {
+  color: red;
+}
+
+.register-link {
+  color: blue;
+  cursor: pointer;
+}
+
+.custom-card {
+  background-color: #f9f5f6;
+}
+</style>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,99 +1,322 @@
 const routes = [
+  //User-admin
+  // PurchaseCart
   {
-    path: "/vendor",
-    component: () => import("components/HeaderSidebarVendor.vue"),
-    name: "HeaderSidebarVendor",
+    path: "/purchase-cart-Admin",
+    name: "PurchaseCartAdminPage",
+    component: () => import("components/HeaderSidebar.vue"),
     children: [
       {
-        path: "vendorprofile",
-        component: () => import("pages/vendor/VendorProfile.vue"),
-        name: "vendorProfile",
+        path: "",
+        component: () => import("pages/User-Admin/PurchaseCartPage.vue"),
       },
+    ],
+  },
+  //Dashboard
+  {
+    path: "/dashboard-Admin",
+    name: "DashboardAdmin",
+    component: () => import("components/HeaderSidebar.vue"),
+    children: [
       {
-        path: "editproduct/:id",
-        component: () => import("pages/vendor/EditProduct.vue"),
-        name: "editProduct",
-        props: true,
+        path: "",
+        component: () => import("pages/User-Admin/IndexPage.vue"),
       },
+    ],
+  },
+  //Purchase-Request
+  {
+    path: "/purchase-request-Admin",
+    name: "PurchaseRequestAdminPage",
+    component: () => import("components/HeaderSidebar.vue"),
+    children: [
       {
-        path: "quodetail/:id",
-        component: () => import("pages/vendor/QuotationDetail.vue"),
-        name: "quotationDetail",
-        props: true,
+        path: "",
+        component: () => import("pages/User-Admin/PurchaseRequestPage.vue"),
       },
+    ],
+  },
+  {
+    path: "/purchase-order-Admin",
+    name: "PurchaseOrderAdminPage",
+    component: () => import("components/HeaderSidebar.vue"),
+    children: [
       {
-        path: "quofix/:id",
-        component: () => import("pages/vendor/QuotationFix.vue"),
-        name: "quotationFix",
-        props: true,
+        path: "",
+        component: () => import("pages/User-Admin/PurchaseOrderPage.vue"),
       },
-
+    ],
+  },
+  //User-Management
+  {
+    path: "/user-management",
+    name: "UserManagementAdminPage",
+    component: () => import("components/HeaderSidebar.vue"),
+    children: [
       {
-        path: "catalogue",
-        component: () => import("pages/CataloguePage.vue"),
-        name: "catalogue",
+        path: "",
+        component: () => import("pages/User-Admin/UserManagementPage.vue"),
       },
+    ],
+  },
+  //Divisi-Departemen-Management
+  {
+    path: "/Divisi-Departemen-Management",
+    name: "DivisiCompanyManagementAdminPage",
+    component: () => import("components/HeaderSidebar.vue"),
+    children: [
       {
-        path: "quotation",
-        component: () => import("pages/vendor/QuotationPage.vue"),
-        name: "quotation",
+        path: "",
+        component: () =>
+          import("pages/User-Admin/DivisiDepartemenManagementPage.vue"),
       },
+    ],
+  },
+  //Add User
+  {
+    path: "/add-user",
+    name: "AddUserAdminPage",
+    component: () => import("components/HeaderSidebar.vue"),
+    children: [
       {
-        path: "order",
-        component: () => import("pages/vendor/OrderPage.vue"),
-        name: "order",
+        path: "",
+        component: () => import("pages/User-Admin/AddUserPage.vue"),
       },
+    ],
+  },
+  //Add-Divisi
+  {
+    path: "/add-divisi",
+    name: "AddDivisiAdminPage",
+    component: () => import("components/HeaderSidebar.vue"),
+    children: [
       {
-        path: "orderdetail/:id",
-        component: () => import("src/pages/vendor/OrderDetail.vue"),
-        name: "orderDetail",
+        path: "",
+        component: () => import("pages/User-Admin/AddDivisiPage.vue"),
       },
-
+    ],
+  },
+  //Add-Departemen
+  {
+    path: "/add-departemen",
+    name: "AddDepartemenAdminPage",
+    component: () => import("components/HeaderSidebar.vue"),
+    children: [
       {
-        path: "product",
-        component: () => import("src/pages/vendor/ProductCatalogues.vue"),
-        name: "product",
+        path: "",
+        component: () => import("pages/User-Admin/AddDepartemenPage.vue"),
       },
+    ],
+  },
+  //Detail-Product
+  {
+    path: "/detail-Admin/:id",
+    component: () => import("components/HeaderSidebar.vue"),
+    children: [
       {
-        path: "addproduct",
-        component: () => import("src/pages/vendor/AddProduct.vue"),
-        name: "addProduct",
-      },
-      {
-        path: "addinfo",
-        component: () => import("src/pages/vendor/AddInfo.vue"),
-        name: "addInfo",
-      },
-
-      {
-        path: "listcatalogue",
-        component: () => import("src/pages/vendor/ListCatalogue.vue"),
-        name: "listCatalogue",
-      },
-
-      {
-        path: "invoice/:id",
-        component: () => import("src/pages/vendor/InvoicePage.vue"),
-        name: "invoicePage",
-      },
-      {
-        path: "product/:id",
-        component: () => import("src/pages/vendor/ProductDetail.vue"),
-        name: "product-detail",
-      },
-      {
-        path: "detail/:id",
-        component: () => import("pages/vendor/ProductDetail.vue"),
+        path: "",
+        component: () => import("pages/User-Admin/DetailPage.vue"),
         name: "detail",
         props: true,
       },
     ],
   },
-  { path: "/login", component: () => import("src/pages/auth/LoginPage.vue") },
+  //Detail PR
   {
-    path: "/register-vendor",
-    component: () => import("src/pages/auth/RegisterVendor.vue"),
-    name: "register-vendor",
+    path: "/detailPR/:id",
+    component: () => import("components/HeaderSidebar.vue"),
+    children: [
+      {
+        path: "",
+        component: () =>
+          import("pages/User-Admin/DetailPurchaseRequestPage.vue"),
+        name: "detailPR",
+        props: true,
+      },
+    ],
+  },
+  {
+    path: "/detailPO/:id",
+    component: () => import("components/HeaderSidebar.vue"),
+    children: [
+      {
+        path: "",
+        component: () => import("pages/User-Admin/DetailPurchaseOrderPage.vue"),
+        name: "detailPO",
+        props: true,
+      },
+    ],
+  },
+  //Detail Quotation
+  {
+    path: "/RequestForQuotation/:id",
+    component: () => import("components/HeaderSidebar.vue"),
+    children: [
+      {
+        path: "",
+        component: () => import("pages/User-Admin/RequestForQuotationPage.vue"),
+        name: "RequestForQuotation",
+        props: true,
+      },
+    ],
+  },
+  //Detail Quotation
+  {
+    path: "/detailRFQ/:id",
+    component: () => import("components/HeaderSidebar.vue"),
+    children: [
+      {
+        path: "",
+        component: () => import("pages/User-Admin/DetailRFQPage.vue"),
+        name: "detailRFQ",
+        props: true,
+      },
+    ],
+  },
+  {
+    path: "/:code/RequestApprovalOrder",
+    component: () => import("components/HeaderSidebar.vue"),
+    children: [
+      {
+        path: "",
+        component: () =>
+          import("pages/User-Admin/RequestApprovalPurchaseOrderPage.vue"),
+        name: "requestApprovalOrder",
+        props: true,
+      },
+    ],
+  },
+  {
+    path: "/detailPRUserApproval/:doc_code",
+    component: () => import("components/HeaderSidebar.vue"),
+    children: [
+      {
+        path: "",
+        component: () =>
+          import("pages/User-Approval/DetailPRUserApprovalPage.vue"),
+        name: "detailPRUserApproval",
+        props: true,
+      },
+    ],
+  },
+  {
+    path: "/detailPOUserApproval/:doc_code",
+    component: () => import("components/HeaderSidebar.vue"),
+    children: [
+      {
+        path: "",
+        component: () =>
+          import("pages/User-Approval/DetailPOUserApprovalPage.vue"),
+        name: "detailPOUserApproval",
+        props: true,
+      },
+    ],
+  },
+  {
+    path: "/:code/RequestApproval",
+    component: () => import("components/HeaderSidebar.vue"),
+    children: [
+      {
+        path: "",
+        component: () => import("pages/User-Admin/RequestApprovalPage.vue"),
+        name: "requestApproval",
+        props: true,
+      },
+    ],
+  },
+  //Edit-Profile
+  {
+    path: "/UserProfile",
+    component: () => import("components/HeaderSidebar.vue"),
+    children: [
+      {
+        path: "",
+        component: () => import("pages/User-Admin/ProfileUserPage.vue"),
+        name: "ProfileUser",
+      },
+    ],
+  },
+  //User-Approval
+  {
+    path: "/purchase-request-approval",
+    name: "PurchaseRequestUserApprovalPage",
+    component: () => import("components/HeaderSidebar.vue"),
+    children: [
+      {
+        path: "",
+        component: () =>
+          import("pages/User-Approval/PurchaseRequestApprovalPage.vue"),
+      },
+    ],
+  },
+  {
+    path: "/purchase-order-approval",
+    name: "PurchaseOrderUserApprovalPage",
+    component: () => import("components/HeaderSidebar.vue"),
+    children: [
+      {
+        path: "",
+        component: () =>
+          import("pages/User-Approval/PurchaseRequestOrderPage.vue"),
+      },
+    ],
+  },
+  //Quotation
+  {
+    path: "/Quotation-Admin",
+    name: "QuotationPage",
+    component: () => import("components/HeaderSidebar.vue"),
+    children: [
+      {
+        path: "",
+        component: () => import("pages/User-Admin/QuotationPage.vue"),
+      },
+    ],
+  },
+  {
+    path: "/Order-Admin",
+    name: "OrderPage",
+    component: () => import("components/HeaderSidebar.vue"),
+    children: [
+      {
+        path: "",
+        component: () => import("pages/User-Admin/OrderPage.vue"),
+      },
+    ],
+  },
+  {
+    path: "/detail-order/:id", // Define the route path with a parameter for the order ID
+    component: () => import("components/HeaderSidebar.vue"),
+    children: [
+      {
+        path: "",
+        component: () => import("pages/User-Admin/DetailOrderPage.vue"),
+        name: "detailOrder", // Set a unique name for the route
+        props: true, // Pass route params as props to the component
+      },
+    ],
+  },
+  {
+    path: "/detail-invoice/:id", // Define the route path with a parameter for the order ID
+    component: () => import("components/HeaderSidebar.vue"),
+    children: [
+      {
+        path: "",
+        component: () => import("pages/User-Admin/DetailInvoicePage.vue"),
+        name: "detailInvoice", // Set a unique name for the route
+        props: true, // Pass route params as props to the component
+      },
+    ],
+  },
+  //Login
+  {
+    path: "/",
+    component: () => import("pages/LoginPage.vue"),
+  },
+  //Register-buyer
+  {
+    path: "/register-buyer",
+    component: () => import("pages/RegisterBuyerPage.vue"),
   },
 ];
 
