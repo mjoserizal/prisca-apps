492b19fa72ee4c4955692914286c0d1d0d0565fa
mjoserizal <jrizal963@student.uns.ac.id>
2024-07-18 22:17:54 +0700

    Update AddDivisiPage.vue

--- src/pages/User-Admin/AddDivisiPage.vue
+++ src/pages/User-Admin/AddDivisiPage.vue
@@ -3,7 +3,7 @@
     <form @submit.prevent="addDivisi" class="bg-white p-6 rounded-md shadow-md">
       <!-- Input Nama Divisi -->
       <div class="mb-4">
-        <label for="name" class="block mb-2 text-sm font-medium text-gray-600">Name Division:</label>
+        <label for="name" class="block mb-2 text-sm font-medium text-gray-600">Name Division :</label>
         <input type="text" id="name" name="name" v-model="divisiName"
           class="block w-full py-2 px-3 border border-gray-300 rounded-md shadow-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
       </div>
