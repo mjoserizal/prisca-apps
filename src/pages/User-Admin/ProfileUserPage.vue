6aaf40b520422e03a7481a1d4597a41e16213c45
mjoserizal <jrizal963@student.uns.ac.id>
2024-07-30 01:51:26 +0700

    UPDATE

--- src/pages/User-Admin/ProfileUserPage.vue
+++ src/pages/User-Admin/ProfileUserPage.vue
@@ -26,7 +26,8 @@
       <div class="mb-4">
         <label for="company_name" class="block mb-2 text-sm font-medium text-gray-600">Company Name:</label>
         <input type="text" id="company_name" name="company_name" v-model="userProfile.company.company_name"
-          class="block w-full py-2 px-3 border border-gray-300 rounded-md shadow-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+          :disabled="userProfile.company.company_name !== ''" class="block w-full py-2 px-3 border border-gray-300 rounded-md shadow-md focus:outline-none
+        focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
       </div>
 
       <!-- Input Alamat Perusahaan -->
