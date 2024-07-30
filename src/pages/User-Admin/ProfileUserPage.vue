15aa6f21e3876036bf2e9e563569e85928f0378f
mjoserizal <jrizal963@student.uns.ac.id>
2024-07-30 12:49:59 +0700

    update

--- src/pages/User-Admin/ProfileUserPage.vue
+++ src/pages/User-Admin/ProfileUserPage.vue
@@ -26,7 +26,7 @@
       <div class="mb-4">
         <label for="company_name" class="block mb-2 text-sm font-medium text-gray-600">Company Name:</label>
         <input type="text" id="company_name" name="company_name" v-model="userProfile.company.company_name"
-          :disabled="userProfile.company.company_name !== ''" class="block w-full py-2 px-3 border border-gray-300 rounded-md shadow-md focus:outline-none
+          :disabled="userProfile.company.company_name !== null && userProfile.company.company_name !== ''" class="block w-full py-2 px-3 border border-gray-300 rounded-md shadow-md focus:outline-none
         focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
       </div>
 
